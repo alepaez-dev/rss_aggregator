6634763f16a3e75c83ac4c142d5ca70ebe400d04
Ale Paez <alepaezdev@gmail.com>
2026-01-14 18:31:52 -0700

    feat: understanding ctx sutff (WIP)

--- internal/tasks/scraper.go
+++ internal/tasks/scraper.go
@@ -4,25 +4,33 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/alepaez-dev/rss_aggregator/internal/database"
 )
 
+// TODO: implement the actual scraping logic here
+func scrapFeed(feed database.Feed) {
+	fmt.Println("Scraping feed:", feed.ID)
+}
+
 func worker(
 	ctx context.Context,
 	jobs <-chan database.Feed,
+	wg *sync.WaitGroup,
 ) {
+
+	defer wg.Done() // worker finished
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case feed, ok := <-jobs:
-			if !ok { // safe
+		case feed, ok := <-jobs: // we received a feed 🙏
+			if !ok { // safe check → is channel closed?
 				return
 			}
-			// TODO: implement the actual scraping logic here
-			fmt.Println("Processing feed:", feed.ID)
+			scrapFeed(feed)
 		}
 	}
 }
@@ -30,18 +38,27 @@ func worker(
 func StartScraping(ctx context.Context, db *database.Queries, concurrency int, interval time.Duration) {
 	ticker := time.NewTicker(interval)
 
+	// cleanup (2nd)
 	defer ticker.Stop()
 
 	jobs := make(chan database.Feed)
 
+	var wg sync.WaitGroup
+	wg.Add(concurrency) // we will wait for N workers to finish
+
 	for i := 0; i < concurrency; i++ {
-		go worker(ctx, jobs)
+		go worker(ctx, jobs, &wg)
 	}
+
+	// cleanup (1st)
+	defer func() {
+		close(jobs) // current jobs keep going but no future jobs
+		wg.Wait()   // WAIT for all workers to finish
+	}()
 
 	for {
 		select {
 		case <-ctx.Done():
-			close(jobs)
 			return
 		case <-ticker.C:
 			feeds, err := db.GetNextFeedsToFetch(ctx, int32(concurrency))
@@ -50,7 +67,11 @@ func StartScraping(ctx context.Context, db *database.Queries, concurrency int, i
 				continue
 			}
 			for _, f := range feeds {
-				jobs <- f
+				select {
+				case <-ctx.Done(): // if ctx is done, stop immediately, in case of deadlock
+					return
+				case jobs <- f: // send job when worker is ready to receive
+				}
 			}
 		}
 	}
